83b49bb2af0c9979d56f9ce4b0c92d5b6e85dae5
Yogendra Gotwal <rverma@gydexp.com>
2024-12-22 06:23:10 +0530

    App.jsx modified: added clearner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import NotePage from "./components/NotePage";
-import BlogPage from "./components/BlogPage";
-import LoginForm from "./components/loginForm";
-import Togglable from "./components/Togglable";
-import Footer from "./components/Footer";
-import noteService from "./services/Notes";
-import loginService from "./services/login";
-import blogService from "./services/Blogs";
-import NoteContent from "./components/NoteContent";
-import { useState, useEffect, useRef } from "react";
+import NotePage from "./components/NotePage.jsx";
+import BlogPage from "./components/BlogPage.jsx";
+import Footer from "./components/Footer.jsx";
+import noteService from "./services/Notes.jsx";
+import loginService from "./services/login.jsx";
+import blogService from "./services/Blogs.jsx";
+import NoteContent from "./components/NoteContent.jsx";
+import { useState, useEffect, createContext } from "react";
 import {
-  BrowserRouter as Router,
   Route,
   Routes,
   Link,
   Navigate,
   useNavigate,
+  useMatch,
 } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+// import "./App.css";
 
-import LoginPage from "./components/LoginPage";
-import BlogContent from "./components/BlogContent";
-import SignUp from "./components/SignUp";
+import LoginPage from "./components/LoginPage.jsx";
+import BlogContent from "./components/BlogContent.jsx";
+import SignUp from "./components/SignUp.jsx";
+import ChatComponent from "./components/ChatComponent.jsx";
 
 const api_key = import.meta.env.VITE_SOME_KEY;
 
+export const UserContext = createContext();
 const App = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState();
+  const [notes, setNotes] = useState([]);
+  const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState(""); //for notification
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
@@ -37,53 +39,12 @@ const App = () => {
       setUser(user);
       noteService.setToken(user.token);
       blogService.setToken(user.token);
+
+      console.log("Current User:", user);
     }
     setLoading(false);
   }, []);
 
-  const handleLogin = async ({ username, password }) => {
-    try {
-      const response = await loginService.login({ username, password });
-      const user = response.data;
-      console.log(user.token);
-      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
-      setUser(user);
-      noteService.setToken(user.token);
-      blogService.setToken(user.token);
-    } catch (error) {
-      setMessage("Wrong credentials");
-      setTimeout(() => {
-        setMessage(null);
-      }, 5000);
-    }
-  };
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    window.localStorage.clear(); // Clears all data from localStorage
-    setUser(null); // Resets the user state
-    navigate("/login");
-  };
-  const logoutButton = () => (
-    <button type="button" onClick={handleLogout}>
-      Logout
-    </button>
-  );
-
-  const loginRef = useRef();
-  const loginForm = () => (
-    <div>
-      {/* <button onClick={showLogin}>Show Login Form</button> */}
-      <Togglable buttonLabel="Log In" ref={loginRef}>
-        <LoginForm handleLogin={(user) => handleLogin(user)} />
-      </Togglable>
-    </div>
-  );
-
-  const Notification = ({ msg }) => {
-    if (msg == null) return null;
-    return <div className="error">{msg}</div>;
-  };
-  const [notes, setNotes] = useState([]);
   useEffect(() => {
     if (user) {
       noteService
@@ -92,6 +53,7 @@ const App = () => {
           const filteredNotes = response.data.filter(
             (note) => note.user && note.user.username === user.username
           );
+          console.log("Notes:", filteredNotes);
           setNotes(filteredNotes);
         })
         .catch((error) => {
@@ -100,13 +62,13 @@ const App = () => {
     }
   }, [user]);
 
-  const [blogs, setBlogs] = useState([]);
   useEffect(() => {
     if (user) {
       blogService
         .getAll()
         .then((response) => {
           const blogs = response.data;
+          console.log("blogs:", blogs);
 
           const filteredBlogs = blogs.filter(
             (blog) => blog.user && blog.user.username === user.username
@@ -118,126 +80,168 @@ const App = () => {
         });
     }
   }, [user]);
+  const handleLogin = async ({ username, password }) => {
+    try {
+      const response = await loginService.login({ username, password });
+      const user = response.data;
+      console.log("Loggin in:", user);
+      console.log("Token set to:", token);
+
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+      setUser(user);
+      noteService.setToken(user.token);
+      blogService.setToken(user.token);
+      navigate("/dashboard", { replace: true });
+    } catch (error) {
+      setMessage("Wrong credentials");
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    window.localStorage.clear();
+    setUser(null);
+    navigate("/login");
+  };
+  const logoutButton = () => (
+    <button type="button" onClick={handleLogout}>
+      Logout
+    </button>
+  );
+
+  const Notification = ({ msg }) => {
+    if (msg == null) return null;
+    return <div className="error">{msg}</div>;
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="app-container">
-      <div className="main-content">
-        <h1 className="heading">Social Media</h1>
-        {message ? <Notification msg={message}></Notification> : null}
-        {user && <p>{user.username} logged-in</p>}
-        {user === null && <Link to="/signup"></Link>}
+    <UserContext.Provider value={{ user: user }}>
+      <div>
+        <div className="main-content">
+          <h1 className="heading">Social Media</h1>
+          {message ? <Notification msg={message}></Notification> : null}
+          {user && <p>{user.username} logged-in</p>}
+          {user === null && <Link to="/signup"></Link>}
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              user ? (
-                <Navigate to="/dashboard"></Navigate>
-              ) : (
-                <div>
-                  <Link to="/login">Log in</Link> |{" "}
-                  <Link to="/signup">Sign up</Link>
-                </div>
-              )
-            }
-          ></Route>
-          <Route
-            path="/signup"
-            element={
-              <div>
-                <p>
-                  Alrady have an account <Link to="/login">Log in</Link>
-                </p>
-                <SignUp />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={
-              user ? (
-                <Navigate to="/dashboard" />
-              ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                user ? (
+                  <Navigate to="/dashboard"></Navigate>
+                ) : (
+                  <div>
+                    <Link to="/login">Log in</Link> |{" "}
+                    <Link to="/signup">Sign up</Link>
+                  </div>
+                )
+              }
+            ></Route>
+            <Route
+              path="/signup"
+              element={
                 <div>
                   <p>
-                    Don't have an account <Link to="/signup">Sign up</Link>
+                    Alrady have an account <Link to="/login">Log in</Link>
                   </p>
-                  <LoginPage handleLogin={handleLogin}></LoginPage>
+                  <SignUp />
                 </div>
-              )
-            }
-          ></Route>
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={
+                user ? (
+                  <Navigate replace to="/dashboard" />
+                ) : (
+                  <div>
+                    <p>
+                      Don't have an account <Link to="/signup">Sign up</Link>
+                    </p>
+                    <LoginPage handleLogin={handleLogin}></LoginPage>
+                  </div>
+                )
+              }
+            ></Route>
 
-          <Route
-            path="/dashboard"
-            element={
-              user ? (
-                <div>
-                  <Link to="/notes">notes</Link> |{" "}
-                  <Link to="/blogs">blogs</Link>
-                </div>
-              ) : (
-                <Navigate to="/login"></Navigate>
-              )
-            }
-          ></Route>
+            <Route
+              path="/dashboard"
+              element={
+                user ? (
+                  <div>
+                    <Link to="/notes">notes</Link> |{" "}
+                    <Link to="/blogs">blogs</Link>| <Link to="/chat">Chat</Link>
+                  </div>
+                ) : (
+                  <Navigate to="/login"></Navigate>
+                )
+              }
+            ></Route>
+            <Route
+              path="/chat"
+              element={<ChatComponent></ChatComponent>}
+            ></Route>
 
-          <Route
-            path="/notes"
-            element={
-              user ? (
-                <div>
-                  <Link to="/blogs">blogs</Link>
-                  <NotePage notes={notes} setNotes={setNotes}></NotePage>
-                </div>
-              ) : (
-                <Navigate to="/login"></Navigate>
-              )
-            }
-          ></Route>
-          <Route
-            path="/blogs"
-            element={
-              user ? (
-                <div>
-                  <Link to="/notes">notes</Link>
-                  <BlogPage blogs={blogs} setBlogs={setBlogs}></BlogPage>
-                </div>
-              ) : (
-                <Navigate to="/login"></Navigate>
-              )
-            }
-          ></Route>
-          <Route
-            path="/notes/:id"
-            element={
-              user ? (
-                <NoteContent notes={notes} />
-              ) : (
-                <Navigate to="/login"></Navigate>
-              )
-            }
-          ></Route>
-          <Route
-            path="/blogs/:id"
-            element={
-              user ? (
-                <BlogContent blogs={blogs} />
-              ) : (
-                <Navigate to="/login"></Navigate>
-              )
-            }
-          ></Route>
-        </Routes>
+            <Route
+              path="/notes"
+              element={
+                user ? (
+                  <div>
+                    <Link to="/blogs">blogs</Link>
+                    <NotePage notes={notes} setNotes={setNotes}></NotePage>
+                  </div>
+                ) : (
+                  <Navigate to="/login"></Navigate>
+                )
+              }
+            ></Route>
+            <Route
+              path="/blogs"
+              element={
+                user ? (
+                  <div>
+                    <Link to="/notes">notes</Link>
+                    <BlogPage blogs={blogs} setBlogs={setBlogs}></BlogPage>
+                  </div>
+                ) : (
+                  <Navigate to="/login"></Navigate>
+                )
+              }
+            ></Route>
+            <Route
+              path="/notes/:id"
+              element={
+                user ? (
+                  <NoteContent notes={notes} />
+                ) : (
+                  <Navigate to="/login"></Navigate>
+                )
+              }
+            ></Route>
+            <Route
+              path="/blogs/:id"
+              element={
+                user ? (
+                  <BlogContent blogs={blogs} />
+                ) : (
+                  <Navigate to="/login"></Navigate>
+                )
+              }
+            ></Route>
+          </Routes>
+        </div>
+        <div>
+          {user && logoutButton()}
+          <Footer></Footer>
+        </div>
       </div>
-      <div>
-        {user && logoutButton()}
-        <Footer></Footer>
-      </div>
-    </div>
+    </UserContext.Provider>
   );
 };
 export default App;
